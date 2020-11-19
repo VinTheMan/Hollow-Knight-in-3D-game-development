a9e11deb8c6f25d7dab65a4faff83aee56426a87
VinTheMan <vincent911016@gmail.com>
2020-11-20 00:25:19 +0800

    Add files via upload

---
+++ hollow/EnemyAi.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI ; 
+public class EnemyAi : MonoBehaviour {
+
+	public float lookRadius = 10f ;
+	public Transform target ;
+	NavMeshAgent agent ;
+	// Use this for initialization
+	void Start () {
+		// target =GetComponent<Transform>() ;
+		agent = GetComponent<NavMeshAgent>() ; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distance = Vector3.Distance(target.position,transform.position) ;
+		if(distance <= lookRadius){
+			FaceTarget();
+			agent.SetDestination(target.position) ;
+			if(distance <= agent.stoppingDistance) {
+				// Attack the target 
+				//face the target
+				FaceTarget();
+			}
+		}
+	}
+
+
+	void FaceTarget () {
+		Vector3 directon = (target.position -transform.position).normalized;
+		Quaternion lookRotation = Quaternion.LookRotation(new Vector3(directon.x, 0,directon.z));
+		transform.rotation = Quaternion.Slerp(this.transform.rotation, lookRotation, Time.deltaTime*5f);
+	}
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red ; 
+		Gizmos.DrawWireSphere(transform.position, lookRadius) ;
+	}
+}
---
+++ hollow/GetSpeedY.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetSpeedY : MonoBehaviour {
+	Rigidbody controller;
+	Animator m_Animator;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<Rigidbody>();
+		m_Animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		// The speed on the x-z plane ignoring any speed
+		float horizontalSpeed = controller.velocity.x;
+		// The speed from gravity or jumping
+		float verticalSpeed  = controller.velocity.y;
+        // The overall speed
+		float overallSpeed = controller.velocity.magnitude;
+
+		if( verticalSpeed <= 0 ) {
+			m_Animator.SetBool("Falling", true) ; 
+		} // if 
+		else {
+			m_Animator.SetBool("Falling", false) ; 
+		} // else 
+	}
+}
